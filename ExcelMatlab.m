21ec26f288b7295222fafe1ca7462379632ef6ab
sbashford <Seth.Bashford@boystown.org>
2017-01-17 14:21:59 -0600

    opening files bug corrected, application works

--- ExcelMatlab.m
+++ ExcelMatlab.m
@@ -3,22 +3,22 @@ classdef ExcelMatlab < handle
         excelApplication
         excelFileWorkbook
         excelFileSheets
-        pathToFile
+        fullPathToFile
     end
     
     methods
         function self = ExcelMatlab(file)
             stack = dbstack('-completenames', 1);
             if size(stack)
-                self.pathToFile = [stack(1).file, '\', file];
+                self.fullPathToFile = [stack(1).file, '\', file];
             else
-                self.pathToFile = [pwd(), '\', file];
+                self.fullPathToFile = [pwd(), '\', file];
             end
             
             self.excelApplication = actxserver('Excel.Application');
             self.excelApplication.DisplayAlerts = false;
             try
-                self.excelFileWorkbook = self.excelApplication.Workbooks.Open(pathToFile); 
+                self.excelFileWorkbook = self.excelApplication.Workbooks.Open(self.fullPathToFile);
             catch
                 self.excelFileWorkbook = self.excelApplication.Workbooks.Add();
             end
@@ -27,7 +27,7 @@ classdef ExcelMatlab < handle
         
         function delete(self)
             try
-                self.excelFileWorkbook.SaveAs(self.pathToFile);
+                self.excelFileWorkbook.SaveAs(self.fullPathToFile);
             catch
                 fprintf('Excel file did not save successfully\n');
             end
@@ -38,7 +38,7 @@ classdef ExcelMatlab < handle
         function writeToSheet(self, data, sheet, topLeftRow, topLeftCol)
             sheetNumber = self.findSheetNumber(sheet);
             if sheetNumber
-                sheetToWrite = self.excelFileSheets(sheetNumber);
+                sheetToWrite = self.excelFileSheets.Item(sheetNumber);
             else
                 numberOfSheets = self.excelFileSheets.Count;
                 self.excelFileSheets.Add([], self.excelFileSheets.Item(numberOfSheets));
@@ -49,7 +49,7 @@ classdef ExcelMatlab < handle
             
             BottomRightCol = size(data, 2) + topLeftCol - 1;
             BottomRightRow = size(data, 1) + topLeftRow - 1;
-            excelRange = getExcelRangeString(topLeftCol, BottomRightCol, topLeftRow, BottomRightRow);
+            excelRange = ExcelMatlab.getExcelRangeString(topLeftCol, BottomRightCol, topLeftRow, BottomRightRow);
             rangeToWrite = get(sheetToWrite, 'Range', excelRange);
             rangeToWrite.Value = data;
         end
@@ -64,6 +64,57 @@ classdef ExcelMatlab < handle
             end
 
             [~, sheetNumber] = ismember(sheet, namesOfSheets);
+        end
+    end
+    
+    methods (Static)
+        function rangeString = getExcelRangeString(firstColumn, lastColumn, firstRow, lastRow)
+        %getExcelRangeString Returns 'RANGE' argument required for xlswrite and xlsread.
+        %   rangeString = getExcelRangeString(firstColumn, lastColumn, firstRow, lastRow)
+        %   returns the string corresponding to the rectangular portion of an excel
+        %   spreadsheet specified by the bounds of the input arguments.
+        %
+        %   Examples:
+        %   
+        %   rangeString = getExcelRangeString(1, 5, 1, 5)
+        %
+        %   rangeString =
+        %
+        %   A1:E5
+        %
+        %   rangeString = getExcelRangeString(6, 9, 4, 7)
+        %
+        %   rangeString =
+        %
+        %   F4:I7
+        %
+        %   Note:
+        %
+        %   Does not support ranges that require three consecutive alphabetic
+        %   letters.  Therefore the maximum value for lastColumn and lastRow is
+        %   26 * 27 = 702.
+
+        firstColumnName = getColumnNameFromColumnNumber(firstColumn);
+        lastColumnName = getColumnNameFromColumnNumber(lastColumn);
+
+        rangeString = [firstColumnName, num2str(firstRow), ':', lastColumnName, num2str(lastRow)];
+
+            function columnName = getColumnNameFromColumnNumber(columnNumber)
+                numberOfLettersInAlphabet = 26;
+
+                if columnNumber > numberOfLettersInAlphabet
+                    counter = 0;
+
+                    while (columnNumber - numberOfLettersInAlphabet > 0)
+                        columnNumber = columnNumber - numberOfLettersInAlphabet;
+                        counter = counter + 1;
+                    end
+
+                    columnName = [char('A' + counter - 1), char('A' + columnNumber - 1)];
+                else
+                    columnName = char('A' + columnNumber - 1);
+                end
+            end
         end
     end
 end
