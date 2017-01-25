8124e6f17585d1dc2fd96e549b4a13d2f9bbefe5
sbashford <Seth.Bashford@boystown.org>
2017-01-25 12:47:08 -0600

    removed sheetWriter class; still not sold on either way

--- ExcelMatlab.m
+++ ExcelMatlab.m
@@ -2,6 +2,7 @@ classdef ExcelMatlab < handle
     properties (Access = 'private')
         app
         workbook
+        workbookSheets
         fullPathToFile
         successSaving = false
     end
@@ -13,6 +14,7 @@ classdef ExcelMatlab < handle
             self.fullPathToFile = fullPathToFile;
             self.startExcel();
             self.openWorkbook();
+            self.workbookSheets = self.workbook.Sheets;
             self.confirmWritableFile();
         end
     end
@@ -47,17 +49,15 @@ classdef ExcelMatlab < handle
             assert(ischar(sheetName), 'Sheet must be a string');
             assert(self.isNonnegativeInteger(topLeftRow) && ...
                    self.isNonnegativeInteger(topLeftCol), 'Row and column must be nonnegative integers.');
-            workbookSheets = self.workbook.Sheets;
-            sheetWriter = ExcelSheetWriter(workbookSheets, sheetName);
-            sheetWriter.write(data, topLeftRow, topLeftCol);
-        end
-        
-        function delete(self)
-            if self.successSaving
-                self.workbook.SaveAs(self.fullPathToFile);
+
+            sheetNumber = self.findSheetNumber(sheetName);
+            if sheetNumber
+                sheetToWrite = self.workbookSheets.Item(sheetNumber);
+            else
+                sheetToWrite = self.addNewSheet(sheetName);
             end
-            Quit(self.app);
-            delete(self.app);
+            excelRangeName = ExcelMatlab.getExcelRangeName(topLeftRow, topLeftCol, data);
+            self.tryWritingToSheet(data, sheetToWrite, excelRangeName);
         end
     end
     
@@ -69,6 +69,77 @@ classdef ExcelMatlab < handle
             catch
                 validity = false;
             end
+        end
+    end
+    
+    methods (Access = 'private')
+        function sheetNumber = findSheetNumber(self, sheetName)
+            numberOfSheets = self.workbookSheets.Count;
+            namesOfSheets = cell(1, numberOfSheets);
+            for i = 1:numberOfSheets;
+                namesOfSheets{i} = self.workbookSheets.Item(i).Name;
+            end
+            [~, sheetNumber] = ismember(sheetName, namesOfSheets);
+        end
+        
+        function newSheet = addNewSheet(self, sheetName)
+            numberOfSheets = self.workbookSheets.Count;
+            self.workbookSheets.Add([], self.workbookSheets.Item(numberOfSheets));
+            numberOfSheets = numberOfSheets + 1;
+            newSheet = self.workbookSheets.Item(numberOfSheets);
+            newSheet.Name = sheetName;
+        end
+    end
+    
+    methods (Static)
+        function rangeName = getExcelRangeName(topLeftRow, topLeftCol, data)
+            BottomRightCol = size(data, 2) + topLeftCol - 1;
+            BottomRightRow = size(data, 1) + topLeftRow - 1;
+            rangeName = ExcelMatlab.getRangeName(topLeftCol, BottomRightCol, topLeftRow, BottomRightRow);
+        end
+    end
+    
+    methods (Static, Access = 'private')
+        function rangeName = getRangeName(firstColumn, lastColumn, firstRow, lastRow)
+            firstColumnName = ExcelMatlab.getColumnNameFromColumnNumber(firstColumn);
+            lastColumnName = ExcelMatlab.getColumnNameFromColumnNumber(lastColumn);
+            rangeName = [firstColumnName, num2str(firstRow), ':', lastColumnName, num2str(lastRow)];
+        end
+        
+        function columnName = getColumnNameFromColumnNumber(columnNumber)
+            numberOfLettersInAlphabet = 26;
+            if columnNumber > numberOfLettersInAlphabet
+                counter = 0;
+                while columnNumber - numberOfLettersInAlphabet > 0
+                    columnNumber = columnNumber - numberOfLettersInAlphabet;
+                    counter = counter + 1;
+                end
+                columnName = [char('A' + counter - 1), char('A' + columnNumber - 1)];
+            else
+                columnName = char('A' + columnNumber - 1);
+            end
+        end
+    end
+    
+    methods (Access = 'private')
+        function tryWritingToSheet(~, data, sheetToWrite, excelRangeName)
+            try
+                rangeToWrite = get(sheetToWrite, 'Range', excelRangeName);
+                rangeToWrite.Value = data;
+            catch MException
+                fprintf('Invalid range specified\n');
+                throw(MException);
+            end
+        end
+    end
+    
+    methods
+        function delete(self)
+            if self.successSaving
+                self.workbook.SaveAs(self.fullPathToFile);
+            end
+            Quit(self.app);
+            delete(self.app);
         end
     end
 end
